ecb5a5f8a61136e6b8ab32495f8ebf37517a027d
Eric K <ericklasson.lbs@gmail.com>
2024-05-29 14:24:05 +0200

    Renames and small fixes

---
+++ Data/Migrations/20240529122046_rename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ForumForGaming.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class rename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "MainCategory",
+                newName: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "MainCategory",
+                newName: "Name");
+        }
+    }
+}
