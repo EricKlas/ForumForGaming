3ba7f69f771e55a1d6f1372aba2f91873072b684
Eric klasson <ericklasson.lbs@gmail.com>
2024-06-02 18:43:05 +0200

    Css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("tr[data-href]").click(function () {
+        window.location.href = $(this).data("href");
+    });
+});
